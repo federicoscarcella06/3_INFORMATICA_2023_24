cbca9c543f03701135057caa3f9bb93d0fe57990
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2024-01-23 10:35:29 +0100

    Update Es_3.java

--- Lab_19_01/Es_3.java
+++ Lab_19_01/Es_3.java
@@ -1,1 +1,23 @@
+import java.util.*;
+import java.io.*;
 
+public class Es_3{
+    public static void main(String[] args) {
+
+        String frase = "", fraseModificata = "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci una stringa: ");
+        frase = scanner.nextLine();
+
+        
+
+        for (int i = 0; i < frase.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                fraseModificata += frase.charAt(i);
+            }
+        }
+
+        System.out.println("Stringa modificata: " + fraseModificata);
+    }
+}
