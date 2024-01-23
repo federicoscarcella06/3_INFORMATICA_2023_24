1db2eb8c6bf4b2386d8790ff0c46b6497cb86f5c
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2024-01-23 17:59:14 +0100

    Update Es_4.java

--- Lab_19_01/Es_4.java
+++ Lab_19_01/Es_4.java
@@ -1,1 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Es_4 {
+    public static void main(String[] args) {
+        String parola = "";
+        int cont = 0;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Inserisci una frase");
+        parola = inp.nextLine();
+
+        for (int i = 0; i < parola.length(); i++) {
+            if (Character.isLetter(parola.charAt(i))) {
+                cont++;
+                while (i < parola.length() && Character.isLetter(parola.charAt(i))) {
+                    i++;
+                }
+            }
+        }
+
+        System.out.println("Il numero di parole nella frase è: " + cont);
+    }
+}
 
