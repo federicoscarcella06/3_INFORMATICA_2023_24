d2a6bbf0b5a2d33fc11a333ff46d7445c2d05070
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2024-01-23 18:13:50 +0100

    Update Es_5.java

--- Lab_19_01/Es_5.java
+++ Lab_19_01/Es_5.java
@@ -1,1 +1,26 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args){
+      
+        String nome="", cognome ="", prime4lettere="",primalettera="";;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Inserisci una il nome ");
+        nome = inp.nextLine();
+      
+        System.out.print("Inserisci una il cognome");
+        cognome = inp.nextLine();
+
+        // Verificare se la lunghezza della stringa
+        if(inputString.length() >= 4){
+
+            prime4Lettere = inputString.substring(0, 4);
+
+            System.out.println("Le prime 4 lettere sono: " + prime4Lettere);
+        }else{
+            System.out.println("La stringa inserita è troppo breve. Inserisci almeno 4 caratteri.");
+        }
+    }
+}
 
