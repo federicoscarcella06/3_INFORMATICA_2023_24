8c5399243a8925ae71c4cecac4864a68ec3f0d66
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2024-02-01 19:52:14 +0100

    Update Es_6.java

--- Lab_19_01/Es_6.java
+++ Lab_19_01/Es_6.java
@@ -1,1 +1,47 @@
+/**
+ * estrae il dominio da una mail
+ * 
+ * @author (Federico scarcella) 
+ * @version (01/02/24)
+ */
+import java.util.*;
+import java.util.*;
+
+public class Es_6 {
+    public static void main(String[] args) {
+        String mail = "", dominio = "";
+        int posLet = -1;
+
+        Scanner inp = new Scanner(System.in);
+        
+        System.out.println("digita stop per terminare il programma");
+        
+        do{
+            System.out.println("Inserisci la tua mail per l'estrazione del dominio:");
+            mail = inp.nextLine();
+
+    
+            posLet = mail.indexOf("@");
+    
+            if(posLet != -1 && !mail.equalsIgnoreCase("stop")) {
+    
+                dominio = mail.substring(posLet + 1);
+    
+                System.out.println("Il dominio della mail è: " + dominio);
+            }else{
+                System.out.println("la mail inserita non è valida");
+            }
+            
+            if(!mail.equalsIgnoreCase("stop")){
+                mail ="";
+            }else{
+                posLet = -1;
+                dominio = "";
+                System.out.println("programma terminato");
+            }
+            
+        }while(!mail.equalsIgnoreCase("stop"));
+    }
+}
+
 
