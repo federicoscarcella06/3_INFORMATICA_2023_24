75088ea915d53cc9a7432a72649e6205db6cb7e4
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2024-03-22 11:00:17 +0100

    Update CreaFile.java

--- BIN/POLDO_ALGORITMO/es_file/CreaFile.java
+++ BIN/POLDO_ALGORITMO/es_file/CreaFile.java
@@ -1,38 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class CreaFile{
-    public static void main(String[] args){
-        File file = new File("dati.txt");
-        Scanner inp = new Scanner(System.in);
+public class CreaFile {
+     public static void main(String[] args){
+
         int n, m;
-        String[] stu;
-        double[][] voti;
+        String s;
+        
+        String stu[];
+        double voti[][];
+        
+        Scanner inp = new Scanner(System.in);
+                
+        File file = new File("dati.txt");
+        
         try{
             FileWriter writer = new FileWriter(file);
             do{
                 System.out.println("Scrivi il numero di studenti:");
                 n = inp.nextInt();
+
             }while(n < 1);
             
-            stu = new String[n];
             do{
-                System.out.println("Scrivi il numero di voti per da assegnare agli studenti");
+                System.out.println("Scrivi il numero di voti da assegnare agli studenti:");
                 m = inp.nextInt();
-            }while(m<0);
-            
-            voti = new double[n][m];
-            
-            for(int i = 0; i < n; i++){
-                System.out.println("Scrivi il nome dello studente:");
-                stu[i] = inp.nextLine();
+            }while (m < 0);
 
-                for(int j=0; j<m;j++){
-                    System.out.println("Scrivi i voti");
-                    voti[i][j] = inp.nextDouble();
-                    writer.append(voti[i][j]+";");
-                }
+            stu = new String[n];
+            voti = new double[n][m];
+
+            for(int i = 0; i < n; i++) {
+                inp.skip("\n");
+                System.out.println("Scrivi il nome dello studente:");
+                stu[i] = inp.nextLine(); 
+                writer.append(stu[i]+" ");
                 
+                for (int j = 0; j < m; j++) {
+                    System.out.println("Scrivi il voto:");
+                    voti[i][j] = inp.nextDouble();
+                    writer.append(voti[i][j] + ";");
+                }
                 writer.append("\n");
             }
             writer.close();
