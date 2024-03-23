be296eeaccfb7d82484a5db4b7404b63e72025bb
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2024-03-24 00:59:00 +0100

    Update codicefiscale.java

--- Lab_23_02_24/codicefiscale.java
+++ Lab_23_02_24/codicefiscale.java
@@ -1,119 +1,88 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-public class codicefiscalefefe{
-    public static void main(String[]args){
-        String nome="", cognome ="",anno,mese,giorno, codicefiscale="",comune="", z="0";
-        int cont =0,contc=0;
-        boolean cogn = false;
+public class CodiceFiscale {
+    public static void main(String[] args) {
+        char csn[];
+        int aa, mm, gg, contc = 0, sost = 0;
+        int indiceCsn = 0; // Contatore per l'array csn
+        int contC = 0; // Contatore delle consonanti
+        char s;
+        String nome, consn, cognome, città, provincia, sesso, cf = "";
+
         Scanner inp = new Scanner(System.in);
-            
-        System.out.println("Inserisci il cognome");
-        cognome=inp.nextLine();
-        
-        System.out.println("Inserisci il nome");
-        nome=inp.nextLine();
-        
-        System.out.println("il giorno di nascita in formato XX");
-        giorno=inp.nextLine(); 
-        
-        System.out.println("il mese di nascita in formato XX");
-        mese=inp.nextLine();
-        
-        System.out.println("inserisci l'anno di nascitain formato XXXX");
-        anno=inp.nextLine();
-        
-        System.out.println("il comune di nascita");
-        comune=inp.nextLine();
-        
-        for(int i=0;i<cognome.length() && cogn == false;i++){
-            if(cognome.charAt(i)=='a' || cognome.charAt(i)=='e' ||cognome.charAt(i)=='i' || cognome.charAt(i)=='o' || cognome.charAt(i)=='u'){
-                                
-            }else{
-                cont ++;
 
-                codicefiscale +=cognome.charAt(i);
-                if(cont >= 3){
-                    cogn = true;
-                }
-            }
-        }
-        cont=0;
-        for(int i=0;i<nome.length() && cont<3;i++){
-            if(nome.charAt(i)=='a' || nome.charAt(i)=='e' ||nome.charAt(i)=='i' || nome.charAt(i)=='o' || nome.charAt(i)=='u'){
-            }
-            else{
+        System.out.println("Inserisci il cognome");
+        cognome = inp.nextLine();
+
+        System.out.println("Inserisci il nome");
+        nome = inp.nextLine();
+
+        cognome = cognome.toLowerCase();
+        nome = nome.toLowerCase();
+
+        // Prima fase===========================
+        for (int i = 0; i < cognome.length(); i++) {
+            if (cognome.charAt(i) != 'a' && cognome.charAt(i) != 'e' && cognome.charAt(i) != 'i' && cognome.charAt(i) != 'o' && cognome.charAt(i) != 'u') {
                 contc++;
             }
         }
-        if(contc>3){
-            for(int i=0;i<nome.length() && cont<4;i++){
-                if(nome.charAt(i)=='a' || nome.charAt(i)=='e' ||nome.charAt(i)=='i' || nome.charAt(i)=='o' || nome.charAt(i)=='u'){
-                }
-                else{
-                    if(cont!=1){
-                        codicefiscale+=nome.charAt(i);
-                    }
+        if (contc > 3) {
+            for (int i = 0, cont = 0; i < cognome.length() && cont < 3; i++) {
+                if (cognome.charAt(i) != 'a' && cognome.charAt(i) != 'e' && cognome.charAt(i) != 'i' && cognome.charAt(i) != 'o' && cognome.charAt(i) != 'u') {
+                    cf += cognome.charAt(i);
                     cont++;
                 }
             }
-        }
-        else{
-            for(int i=0;i<nome.length();i++){
-                if(nome.charAt(i)=='a' || nome.charAt(i)=='e' ||nome.charAt(i)=='i' || nome.charAt(i)=='o' || nome.charAt(i)=='u'){
-                }
-                else{
-                    codicefiscale+=nome.charAt(i);
-                }
+        } else if (contc < 3 && cognome.length() <= 3) {
+            sost = 3 - cognome.length();
+            for (int i = 0; i < cognome.length(); i++) {
+                cf += cognome.charAt(i);
+            }
+            for (int j = 0; j < sost; j++) {
+                cf += "X";
             }
         }
-        for(int i=anno.length()-2;i<anno.length();i++){
-            codicefiscale+=anno.charAt(i);
-        }
+        contc = 0;
+        // Seconda fase===========================ES CF SCRFRC06E03L781S
         
-        switch(mese){
-            case "01":codicefiscale+="A";
-                break;
-            case "02":codicefiscale+="B";
-                break;
-            case "03":codicefiscale+="C";
-                break;
-            case "04":codicefiscale+="D";
-                break;
-            case "05":codicefiscale+="E";
-                break;
-            case "06":codicefiscale+="H";
-                break;
-            case "07":codicefiscale+="L";
-                break;
-            case "08":codicefiscale+="M";
-                break;
-            case "09":codicefiscale+="P";
-                break;
-            case "10":codicefiscale+="R";
-                break;
-            case "11":codicefiscale+="S";
-                break;
-            case "12":codicefiscale+="T";
-                break;
+        for (int i = 0; i < nome.length(); i++) {
+            if (nome.charAt(i) != 'a' && nome.charAt(i) != 'e' && nome.charAt(i) != 'i' && nome.charAt(i) != 'o' && nome.charAt(i) != 'u') {
+                contC++;
+            }
         }
-             
-        codicefiscale+=giorno;
-        comune = comune.toLowerCase();
+        csn = new char[contC];
         
-        switch(comune){
-            case "verona":codicefiscale+="L781";
-                break;
-            case "milano":codicefiscale+="F205";
-                break;
-            case "torino":codicefiscale+="L219";
-                break;
-            case "roma":codicefiscale+="H501";
-                break;
-            case "napoli":codicefiscale+="F839";
-                break;
+        for (int i = 0; i < nome.length(); i++) {
+            if (nome.charAt(i) != 'a' && nome.charAt(i) != 'e' && nome.charAt(i) != 'i' && nome.charAt(i) != 'o' && nome.charAt(i) != 'u') {
+                csn[indiceCsn++] = nome.charAt(i);
+            }
         }
-        codicefiscale = codicefiscale.toUpperCase();
-        System.out.println(codicefiscale);
+        if (contC > 3 ) {
+            cf += csn[0];
+            cf += csn[2];
+            cf += csn[3];
+        }else if(contC <3 || nome.length() >= 3 ){ // problema
+            for (int i = 0; i < cognome.length(); i++){
+                if (cognome.charAt(i) != 'a' && cognome.charAt(i) != 'e' && cognome.charAt(i) != 'i' && cognome.charAt(i) != 'o' && cognome.charAt(i) != 'u') {
+                    contc++;
+                }
+            }
+        }else if (contc > 3) {
+            for (int i = 0, cont = 0; i < cognome.length() && cont < 3; i++) {
+                if (cognome.charAt(i) != 'a' && cognome.charAt(i) != 'e' && cognome.charAt(i) != 'i' && cognome.charAt(i) != 'o' && cognome.charAt(i) != 'u') {
+                    cf += cognome.charAt(i);
+                    cont++;
+                }
+            }  
+        }else if(contC < 3 && nome.length() <= 3) {
+            sost = 3 - nome.length();
+            for (int i = 0; i < nome.length(); i++) {
+                cf += nome.charAt(i);
+            }
+            for (int j = 0; j < sost; j++) {
+                cf += "X";
+            }
+        }
+        System.out.println(cf);
     }
 }
