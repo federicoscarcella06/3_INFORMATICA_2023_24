d591cae7ae66f0a9e40e288ee3b85e2e6b61a6e6
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2025-01-13 14:01:36 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[]args){
+        
+        VerificaData vr = new VerificaData(31,2,2025);
+        
+        System.out.println(vr.getGg() +"/"+ vr.getMm() +"/"+ vr.getAaaa());
+        
+        
+    }
+
+}
+
