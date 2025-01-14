dd2be668182327ecefc2815bcfeb4d6759ee40a2
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2025-01-14 21:01:55 +0100

    Update VerificaData.java

--- VerificaData.java
+++ VerificaData.java
@@ -1,25 +1,20 @@
-
-
-
-
-public class VerificaData{
+public class VerificaData {
     private int gg;
     private int mm;
     private int aaaa;
-    
-    public VerificaData(){
+
+    public VerificaData() {
         gg = 0;
         mm = 0;
         aaaa = 0;
-    
-    } 
-    
-    public VerificaData(int giorno, int mese, int anno){
+    }
+
+    public VerificaData(int giorno, int mese, int anno) {
         int[] giorniInMese = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         boolean dataValida = false;
-        
-        if(mese >= 1 && mese <= 12 && anno >= 0){
-            switch(mese){
+
+        if (mese >= 1 && mese <= 12 && anno >= 0) {
+            switch (mese) {
                 case 1, 3, 5, 7, 8, 10, 12:
                     dataValida = giorno >= 1 && giorno <= 31;
                     break;
@@ -33,26 +28,88 @@ public class VerificaData{
                     dataValida = false;
                     break;
             }
-        }else{
+        } else {
             dataValida = false;
         }
-        if(dataValida){
+        if (dataValida) {
             this.gg = giorno;
             this.mm = mese;
             this.aaaa = anno;
-        }else{
+        } else {
             this.gg = -1;
             this.mm = -1;
-            this.aaaa = -1;    
+            this.aaaa = -1;
         }
     }
-    public int getGg(){
+
+    public int getGg() {
         return this.gg;
     }
-    public int getMm(){
+
+    public int getMm() {
         return this.mm;
-    }    
-    public int getAaaa(){
+    }
+
+    public int getAaaa() {
         return this.aaaa;
-    }    
+    }
+
+    public String verifica(int giorno, int mese, int anno, int giorno2, int mese2, int anno2) {
+        boolean dataValida1 = false;
+        boolean dataValida2 = false;
+        String vera = "data valida";
+        String falsa = "una delle date non è valida";
+
+        // Verifica la validità della prima data
+        if (mese >= 1 && mese <= 12 && anno >= 0) {
+            switch (mese) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    dataValida1 = giorno >= 1 && giorno <= 31;
+                    break;
+                case 4, 6, 9, 11:
+                    dataValida1 = giorno >= 1 && giorno <= 30;
+                    break;
+                case 2:
+                    dataValida1 = (anno % 4 == 0 && giorno >= 1 && giorno <= 29) || (giorno >= 1 && giorno <= 28);
+                    break;
+                default:
+                    dataValida1 = false;
+                    break;
+            }
+        } else {
+            dataValida1 = false;
+        }
+
+        // Verifica la validità della seconda data
+        if (mese2 >= 1 && mese2 <= 12 && anno2 >= 0) {
+            switch (mese2) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    dataValida2 = giorno2 >= 1 && giorno2 <= 31;
+                    break;
+                case 4, 6, 9, 11:
+                    dataValida2 = giorno2 >= 1 && giorno2 <= 30;
+                    break;
+                case 2:
+                    dataValida2 = (anno2 % 4 == 0 && giorno2 >= 1 && giorno2 <= 29) || (giorno2 >= 1 && giorno2 <= 28);
+                    break;
+                default:
+                    dataValida2 = false;
+                    break;
+            }
+        } else {
+            dataValida2 = false;
+        }
+
+        // Se entrambe le date sono valide
+        if (dataValida1 && dataValida2) {
+            // Confronto tra le due date
+            if (anno2 > anno || (anno2 == anno && mese2 > mese) || (anno2 == anno && mese2 == mese && giorno2 >= giorno)) {
+                return "il prodotto non è ancora scaduto";
+            } else {
+                return "il prodotto è scaduto";
+            }
+        } else {
+            return falsa; //una data non è valida
+        }
+    }
 }
