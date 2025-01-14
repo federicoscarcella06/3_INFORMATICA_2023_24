54b651cca37a762f944020e9f37de94b9e107750
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2025-01-14 21:02:07 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
-public class Main{
-    public static void main(String[]args){
+public class Main {
+    public static void main(String[] args) {
         
-        VerificaData vr = new VerificaData(31,2,2025);
-        
-        System.out.println(vr.getGg() +"/"+ vr.getMm() +"/"+ vr.getAaaa());
-        
-        
+       Scanner scanner = new Scanner(System.in);
+       VerificaData verificaData = new VerificaData();
+       
+       int giorno1 = 0, mese1 = 0, anno1 = 0;
+       int giorno2 = 0, mese2 = 0, anno2 = 0;
+
+       System.out.println("Inserisci la prima data:");
+       System.out.print("Giorno: ");
+       giorno1 = scanner.nextInt();
+       System.out.print("Mese: ");
+       mese1 = scanner.nextInt();
+       System.out.print("Anno: ");
+       anno1 = scanner.nextInt();
+
+       System.out.println("Inserisci la seconda data:");
+       System.out.print("Giorno: ");
+       giorno2 = scanner.nextInt();
+       System.out.print("Mese: ");
+       mese2 = scanner.nextInt();
+       System.out.print("Anno: ");
+       anno2 = scanner.nextInt();
+
+ 
+       String risultato = verificaData.verifica(giorno1, mese1, anno1, giorno2, mese2, anno2);
+
+       System.out.println("Risultato: " + risultato);
     }
-
 }
-
