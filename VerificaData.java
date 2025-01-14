ff7288d1466541faeb967d8017d76f3bb86d3daa
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2025-01-14 21:09:18 +0100

    Update VerificaData.java

--- VerificaData.java
+++ VerificaData.java
@@ -2,16 +2,23 @@ public class VerificaData {
     private int gg;
     private int mm;
     private int aaaa;
+    private int gg2;
+    private int mm2;
+    private int aaaa2;
 
     public VerificaData() {
         gg = 0;
         mm = 0;
         aaaa = 0;
+        gg2 = 0;
+        mm2 = 0;
+        aaaa2 = 0;
     }
 
-    public VerificaData(int giorno, int mese, int anno) {
+    public VerificaData(int giorno, int mese, int anno int giorno2, int mese2, int anno2) {
         int[] giorniInMese = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         boolean dataValida = false;
+        boolean dataValida2 = false;
 
         if (mese >= 1 && mese <= 12 && anno >= 0) {
             switch (mese) {
@@ -31,6 +38,27 @@ public class VerificaData {
         } else {
             dataValida = false;
         }
+
+
+        if (mese2 >= 1 && mese2 <= 12 && anno2 >= 0) {
+            switch (mese2) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    dataValida2 = giorno2 >= 1 && giorno2 <= 31;
+                    break;
+                case 4, 6, 9, 11:
+                    dataValida2 = giorno2 >= 1 && giorno2 <= 30;
+                    break;
+                case 2:
+                    dataValida2 = (anno2 % 4 == 0 && giorno2 >= 1 && giorno2 <= 29) || (giorno2 >= 1 && giorno2 <= 28);
+                    break;
+                default:
+                    dataValida2 = false;
+                    break;
+            }
+        } else {
+            dataValida2 = false;
+        }
+        
         if (dataValida) {
             this.gg = giorno;
             this.mm = mese;
@@ -39,6 +67,16 @@ public class VerificaData {
             this.gg = -1;
             this.mm = -1;
             this.aaaa = -1;
+        }
+        
+        if (dataValida2) {
+            this.gg2 = giorno2;
+            this.mm2 = mese2;
+            this.aaaa2 = anno2;
+        } else {
+            this.gg2 = -1;
+            this.mm2 = -1;
+            this.aaaa2 = -1;
         }
     }
 
